935612f7d35d1f289ff006f573fe2a14685e3f64
Aadityavv <aadityavv9@gmail.com>
2024-07-17 22:21:40 +0530

    Basic js file added

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.render("login.ejs");
+})
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`);
+})
